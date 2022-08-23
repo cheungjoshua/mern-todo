4d2ea2dc241c2c8f47be52646a87665dfff1b8ab
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-23 16:14:23 -0700

    add connection to cloud mongoDB

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
+const PASSWORD = process.env.DB_USER_PASSWORD;
+const USER = process.env.DB_USER;
+
+//mid ware
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-const port = 8080;
+// Connect to DB
+mongoose.connect(
+  `mongodb+srv://${USER}:${PASSWORD}@cluster0.w7eg26k.mongodb.net/todoDB`,
+  { useNewUrlParser: true },
+  () => {
+    console.log("conneted to DB");
+  }
+);
 
+// Routes
 app.get("/", (req, res) => {
   res.send("root ");
 });
+
+// Port
+const port = 8080;
 
 app.listen(port, () => {
   console.log("server start at 8080");
