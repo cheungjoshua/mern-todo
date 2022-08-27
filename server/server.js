e08d497919258f7ce062cbeca6b2dc189528bb2e
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-26 17:56:27 -0700

    minor fix

--- server/server.js
+++ server/server.js
@@ -3,37 +3,29 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+
 require("dotenv/config");
 
 // Import Route
 const todosRoute = require("./routes/todos");
 
 // Middleware
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("It is root, nothing here!");
-});
-
-app.use("/todos", todosRoute);
+app.use("/api/todos", todosRoute);
 
 // Connect to DB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log("conneted to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("conneted to DB"))
+  .catch((err) => console.log(err));
 
 // Listen Port
-const port = 8080;
 
-app.listen(port, () => {
-  console.log("server start at 8080");
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`server start at ${process.env.PORT}`);
 });
