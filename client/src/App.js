082c4b4ea69f405e7813a8dc1373269d567a7c5f
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-31 13:28:46 -0700

    add fetchTodos function

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import { Button, Form, Row, Col } from "react-bootstrap";
 
 function App() {
   const [post, setPost] = useState({
-    title: "",
-    description: "",
+    todo: "",
   });
+  const [todosList, setTodoList] = useState([]);
 
   useEffect(() => {
-    console.log(post);
-  }, [post]);
+    fetchTodos();
+    console.log(todosList);
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const todosList = await axios.get("/api/todos");
+      setTodoList(todosList.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,36 +32,43 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="d-flex flex-column justify-content-center align-items-center">
-        <h1>Todo app</h1>
-      </nav>
-      <div className="d-flex flex-column justify-content-center align-items-center">
-        <Form>
-          <Row>
-            <Col sm={10}>
-              <Form.Group>
-                <Form.Control
-                  name="title"
-                  value={post.title}
-                  onChange={handleChange}
-                  placeholder="Title"
-                  className="mb-3"
-                />
-                {/* <Form.Control
+      <Row>
+        <nav className="d-flex flex-column justify-content-center align-items-center">
+          <h1>Todo app</h1>
+        </nav>
+      </Row>
+      <Row>
+        <div className="form__container d-flex flex-column justify-content-center align-items-center">
+          <Form>
+            <Row>
+              <Col sm={10}>
+                <Form.Group>
+                  <Form.Control
+                    name="todo"
+                    value={post.todo}
+                    onChange={handleChange}
+                    placeholder="TODO"
+                    className="mb-3"
+                  />
+                  {/* <Form.Control
               name="description"
               value={post.description}
               onChange={handleChange}
               placeholder="Description"
               className="mb-3"
             /> */}
-              </Form.Group>
-            </Col>
-            <Col sm={1}>
-              <Button onClick={() => {}}>Create</Button>
-            </Col>
-          </Row>
-        </Form>
-      </div>
+                </Form.Group>
+              </Col>
+              <Col sm={1}>
+                <Button onClick={() => {}}>Create</Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </Row>
+      <Row>
+        <div className="todo-display__container"></div>
+      </Row>
     </div>
   );
 }
