a2327773ae4f702dd8f06ed786dc6db835d9301a
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-31 20:44:06 -0700

    succes pass fetch data to todoListItem

--- client/src/components/TodosList.jsx
+++ client/src/components/TodosList.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
+import TodosListItem from "./TodosListItem";
 import { todosListContext } from "../contexts/TodoProvider.jsx";
 
 export default function TodosList() {
   const { todosList, setTodosList } = useContext(todosListContext);
-  console.log(typeof todosList);
-  console.log(todosList);
 
-  return <div>TodosList</div>;
+  const todos = todosList.map((item) => {
+    return <TodosListItem key={item._id} todo={item} />;
+  });
+
+  return <>{todos}</>;
 }
