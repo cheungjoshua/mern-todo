6b20ab9959a58cc303091885479c72b4a1d76168
Joshua C <contact.cjoshua@gmail.com>
2022-09-07 16:12:19 -0700

    add button in todoListItem

--- client/src/components/TodosListItem.jsx
+++ client/src/components/TodosListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Row, Col } from "react-bootstrap";
 
 export default function TodosListItem({ todo, setTodosList }) {
   const date = new Date(todo.date);
@@ -9,5 +10,22 @@ export default function TodosListItem({ todo, setTodosList }) {
     timeZone: "PST",
   });
   console.log(time);
-  return <div>{todo.todo}</div>;
+  return (
+    <>
+      <Row className="">
+        <Col sm={1}>
+          <Button>DONE</Button>
+        </Col>
+        <Col sm={2}>
+          <div>{todo.todo}</div>
+        </Col>
+        <Col sm={1}>
+          <Button>EDIT</Button>
+        </Col>
+        <Col sm={1}>
+          <Button>REMOVE</Button>
+        </Col>
+      </Row>
+    </>
+  );
 }
